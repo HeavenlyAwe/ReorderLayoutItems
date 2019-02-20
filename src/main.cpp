c227e0b6728e73a72be627fefc895240985ba035
Christoffer Fridlund <christoffer.fridlund@helsinki.fi>
2019-02-20 18:44:37 +0200

    Fix segfaults when using wrong commands.

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,22 @@
 
 #include <iostream>
 
+void help(QString message, QString arg) {
+  std::cerr << "  " << message.toStdString() << std::endl;
+  std::cerr << "  " << arg.toStdString() << " " << "<ui file> [--verbose] [--backup]" << std::endl;
+}
+
 int main(int argc, char *argv[]) {
   DomParser parser;
 
   QString path = "";
-  for (int i = 1; i < argc; ++i) {
+  if (argc < 2) {
+    help("Please give the UI file to reorder.", argv[0]);
+    return 1;
+  }
+  path = QString(argv[1]);
+
+  for (int i = 2; i < argc; ++i) {
     if (QString(argv[i]) == "--verbose") {
       parser.setVerbose(true);
 
@@ -15,12 +26,12 @@ int main(int argc, char *argv[]) {
       parser.setBackup(true);
 
     } else if (QString(argv[i]) == "--help" || QString(argv[i]) == "-h") {
-      std::cerr << "Please give the UI file to reorder." << std::endl;
-      std::cerr << argv[0] << "<ui file> [--verbose]" << std::endl;
-      return 1;
+      help("Help", argv[0]);
+      return 0;
 
     } else {
-      path = QString(argv[i]);
+      help("Unknown command", argv[0]);
+      return 1;
     }
   }
 
